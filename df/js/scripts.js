bfd4cb518aa0cad9ca42f8b5e18ab76253998512
Sascha Duenkelberg <DuenkeS@basfad.basf.net>
2023-08-16 09:33:19 +0200

    added scroll to top button

--- df/js/scripts.js
+++ df/js/scripts.js
@@ -6,63 +6,6 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
-
-// Custom validation
-const form = document.getElementById('contactForm');
-const nameInput = document.getElementById('name');
-const emailInput = document.getElementById('email');
-const phoneInput = document.getElementById('phone');
-const messageInput = document.getElementById('message');
-
-form.addEventListener('submit', (event) => {
-    if (!form.checkValidity()) {
-        event.preventDefault();
-        event.stopPropagation();
-    }
-
-    // Name validation
-    if (nameInput.value.trim() === '') {
-        nameInput.setCustomValidity('Bitte gib deinen Namen ein.');
-    } else {
-        nameInput.setCustomValidity('');
-    }
-
-    // Email validation
-    if (emailInput.value.trim() === '') {
-        emailInput.setCustomValidity('Bitte gib eine gültige Email-Adresse ein.');
-    } else {
-        emailInput.setCustomValidity('');
-    }
-
-    // Phone validation
-    if (phoneInput.value.trim() === '') {
-        phoneInput.setCustomValidity('Bitte gib eine Telefonnummer ein.');
-    } else {
-        phoneInput.setCustomValidity('');
-    }
-
-    // Message validation
-    if (messageInput.value.trim() === '') {
-        messageInput.setCustomValidity('Bitte gib eine Nachricht ein.');
-    } else {
-        messageInput.setCustomValidity('');
-    }
-
-    form.classList.add('was-validated');
-});
-
-// Open email client
-form.addEventListener('click', () => {
-    const email = "post@digitalfuchs.net";
-    const subject = encodeURIComponent('Kontaktanfrage');
-    const body = encodeURIComponent(
-        `Name: ${nameInput.value}\nEmail: ${email}\nTelefon: ${phoneInput.value}\nNachricht: ${messageInput.value}`
-    );
-
-    form.action = `mailto:${email}?subject=${subject}&body=${body}`;
-    form.target = '_blank';
-});
-
 const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
 // Check scroll position and toggle button visibility
